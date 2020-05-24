b112e7bdaa0d5680bff14d028b2365a76d6a6bbf
MarZapatta <mar.zapatta@gmail.com>
2020-05-24 08:29:56 -0500

    se añade a nueva carpeta

---
+++ ejemplo01.R
@@ -0,0 +1,13 @@
+# Comentario
+#En Rstudio la combinacion de teclas CTRL+SHIFT+N apertura un nuevo editor
+#Modifico/seteo configuro el directorio de trabajo
+setwd("E:/Escritorio/Clase2R4DS")
+
+# verificar wd wiorking directory
+getwd()
+
+#listar el contenido
+dir()
+help(dir)
+dir(pattern = "csv")
+dir(pattern = "R")
