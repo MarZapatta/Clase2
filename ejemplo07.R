5b06e6b2cf411864c38affcec88b12df34ee0a52
MarZapatta <mar.zapatta@gmail.com>
2020-05-24 11:33:36 -0500

    se agrego detalles al ejemplo07.R

--- ejemplo07.R
+++ ejemplo07.R
@@ -23,4 +23,59 @@ retail <- na.omit(retail)
 str(retail)
 
 #### retail:analisis por año ####
+#veamos cuanto es lo que produjo de ventas por año
+retail
+retail[retail$Year==2000, ]
+class(retail[retail$Year==2000, ])
+retail[retail$Year==2000, ]$Sales
+sum(retail[retail$Year==2000, ]$Sales)
+unique(retail$Year)[1]
+unique(retail$Year)[5:9]
+
+#utilicemos una estructura for para crear un data frame donde
+#almacenaremos un resumen por año
+
+ResumenSales <- data.frame(Yea= integer(), VentaTotal= double())
+NuevaFila <- data.frame()
+
+for(y in unique(retail$Year)){
+  NuevaFila <- data.frame(Year = y, VentaTotal= sum(retail[retail$Year==y, ]$Sales))
+  ResumenSales <- rbind(ResumenSales , NuevaFila)
+  }
+
+
+plot(x= ResumenSales$Year, y=ResumenSales$VentaTotal)
+
+#### Comportamiento de la data por mes ####
+retail[retail$Month == "Jan", ]$Sales
+unique(retail$Month)
+
+for(m in retail$Month){
+  print(retail[retail$Month == m, ])
+}
+
+for(m in retail$Month){
+  print(m)
+}
+
+help("aggregate")
+RetailMes <- aggregate(Sales ~ Month, data = retail, FUN = sum)
+RetailMes
+
+month.abb
+month.name
+
+RetailMes <- RetailMes[order(match(RetailMes$Month, month.abb)), ]
+
+
+
+
+
+
+
+
+
+
+
+
 
